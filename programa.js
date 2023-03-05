2ba910dd7dbc750f338b75bef46430f45bf0dfba
johanna estupiñan <ft.johannamanrique@gmail.com>
2023-03-05 11:10:17 -0500

    Intro JS code

---
+++ programa.js
@@ -0,0 +1,35 @@
+
+let a = 34;
+let b = 50
+console.log(a*b);
+
+// Taller: Menor valor de dos variables
+console.log("Taller: Menor valor de dos variables");
+let c = 25;
+let d = 38;
+console.log("c =", c);
+console.log("d =", d);
+
+if (c < d) {
+    console.log("El numero menor es c =", c);
+} else {
+    console.log("El numero menor es d =", d);
+}
+
+
+// Taller: Mayor valor de tres variables
+console.log("Taller: Mayor valor de tres variables");
+let e = 98;
+let f = 105;
+let g = 100;
+console.log("e =", e);
+console.log("f =", f);
+console.log("g =", g);
+
+if (e > f && e > g) {
+    console.log("El numero mayor es e =", e);
+} else if (f > e && f > g) {
+    console.log("El numero mayor es f =", f);
+} else {
+    console.log("El numero mayor es g =", g);
+}
